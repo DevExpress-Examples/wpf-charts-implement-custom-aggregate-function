c6659c72787ab7c5a84855903d4c45d755389ff6
CORP\karpov.roman <roman.karpov@devexpress.com>
2018-05-28 17:07:46 +0300

    The first example commit.

---
+++ CS/DataAggregationSample/Model/CurrencyRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAggregationSample.Model {
+    class CurrencyRate {
+        public DateTime Date { get; private set; }
+        public double Value { get; private set; }
+
+        public CurrencyRate(DateTime date, double value) {
+            this.Date = date;
+            this.Value = value;
+        }
+    }
+}
---
+++ CS/DataAggregationSample/View/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Windows;
+using DevExpress.Xpf.Charts;
+
+namespace DataAggregationSample.View {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+
+    #region #CustomAggregateFunctionImplementation
+    class FinancialAggregateFunction : CustomAggregateFunction {
+        public override double[] Calculate(GroupInfo groupInfo) {
+            return new double[] {
+                groupInfo.Values1.Max(),
+                groupInfo.Values1.Min(),
+                groupInfo.Values1.First(),
+                groupInfo.Values1.Last()
+            };
+        }
+    }
+    #endregion #CustomAggregateFunctionImplementation
+}
